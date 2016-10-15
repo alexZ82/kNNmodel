ad2ef8ab2f620f3c13ccafa7f387d252f4e6d9c9
phil8192 <phil@parasec.net>
2016-10-15 22:14:50 +0300

    fix localNeighbourhoodWithPruningComplete

--- kNNmodelProjectFunctions.R
+++ kNNmodelProjectFunctions.R
@@ -16,7 +16,37 @@ localInstances<-function(distances,labels){
   return(sort(instances))
 }
 
-localNeighbourhoodWithPruningComplete<-function(distances,r,labels){
+##' Instance local neighbourhood. (Step 3 in the paper)
+##'
+##' "For each ‘ungrouped’ data tuple, find its largest local neighbourhood which
+##'  covers the largest number of neighbours with the same category."
+##'
+##' The local neighbourhood for an instance is the largest circle around an 
+##' instance which contains only instances of the same class (if r=0). With
+##' increasing r, the local neighbourhood may contain instances from other 
+##' classes. 
+##'
+##' @param distances The distances from this instance to all other instances 
+##'                  including itself.
+##' @param r         Error tolerance degree. Where r=0, the local neighbourhood 
+##'                  will only contain instances of the same class. If r=1, the
+##'                  local neighbourhood may contain 1 instance from another
+##'                  class. 
+##' @param labels    The class labels corresponding to the distance vector.       
+##' @return The neighbourhood class, distance to the outermost instance in this
+##'         neighbourhood and the number of instances inside the neighbourhood.
+##'
+##' @examples
+##'
+##' # cluster.size = 6
+##' localNeighbourhoodWithPruningComplete(c(1,2,3,4,5,6), 0, c(1,1,1,1,1,1)) 
+##'
+##' # cluster.size = 1
+##' localNeighbourhoodWithPruningComplete(c(1,2,3,4,5,6), 0, c(2,1,1,1,1,1))
+##'
+##' # cluster size = 3
+##' localNeighbourhoodWithPruningComplete(c(1,2,3,4,5,6), 1, c(1,2,1,3,3,3))
+localNeighbourhoodWithPruningComplete<-function(distances,r=0,labels){
   temp<-data.frame(distances,labels)
   temp<-temp[order(temp$distances),]
   groups<-rle(temp$labels)
@@ -25,9 +55,9 @@ localNeighbourhoodWithPruningComplete<-function(distances,r,labels){
   tmpN<-which(tmp!=0) # which ones are different
   place<-tail(which(cumsum(groups$lengths[tmpN])<=r),1)
   if(length(place)>0){
-    cluster.size = sum(groups$lengths[1:(tmpN[place]+1)])
+    cluster.size <- sum(groups$lengths[1:min(length(groups$lengths), tmpN[place]+1)])
   }else{
-    cluster.size = groups$lengths[1]
+    cluster.size <- groups$lengths[1]
   }
   cluster.distance<-temp$distances[cluster.size]
   cluster.class<-groups$values[1]
@@ -52,9 +82,7 @@ globalNeighbourhood<-function(distances,labels){
     clstr<-clstr+1
     tmp<- subset(theLocals, bound=='u')
     tmp <- tmp[with(tmp, order(-cluster.size,cluster.distance)),]
-    head(tmp)
     clusters[clstr,]<-as.matrix(tmp[1,1:4])
-    head(clusters)
     clusterMembers<-localInstances(distances[clusters[clstr,4],],labels)
     theLocals$bound[theLocals$instances %in% c(clusterMembers)]<-'b'
   }
